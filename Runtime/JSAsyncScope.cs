50818aa05d9ad674d8f1707ef9927321714caf70
Vladimir Morozov <vmoroz@users.noreply.github.com>
2023-02-16 14:26:43 -0800

    Fix formatting/design issue with the Dispose

--- Runtime/JSAsyncScope.cs
+++ Runtime/JSAsyncScope.cs
@@ -27,6 +27,13 @@ namespace NodeApi
 
         public void Dispose()
         {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
             if (!IsDisposed)
             {
                 IsDisposed = true;
