d0ac0c5a2879893038a71e01560589210a261c54
Jason Ginchereau <jasongin@microsoft.com>
2023-02-17 00:02:56 -1000

    Perf comparison

---
+++ Test/TestCases/edgejs-perf/Edge.Performance.cs
@@ -0,0 +1,53 @@
+// Ported from https://github.com/agracio/edge-js/blob/master/performance/performance.cs
+
+using System;
+using NodeApi;
+
+
+namespace Edge.Performance
+{
+    [JSExport]
+    public static class Startup
+    {
+        public static Book Invoke(Book input)
+        {
+            var book = new Book
+            {
+                Title = "Run .NET and node.js in-process with edge.js",
+                Author = new Person
+                {
+                    First = "Anonymous",
+                    Last = "Author"
+                },
+                Year = 2013,
+                Price = 24.99,
+                Available = true,
+                Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus posuere tincidunt felis, et mattis mauris ultrices quis. Cras molestie, quam varius tincidunt tincidunt, mi magna imperdiet lacus, quis elementum ante nibh quis orci. In posuere erat sed tellus lacinia luctus. Praesent sodales tellus mauris, et egestas justo. In blandit, metus non congue adipiscing, est orci luctus odio, non sagittis erat orci ac sapien. Proin ut est id enim mattis volutpat. Vivamus ultrices dapibus feugiat. In dictum tincidunt eros, non pretium nisi rhoncus in. Duis a lacus et elit feugiat ullamcorper. Mauris tempor turpis nulla. Nullam nec facilisis elit.",
+                Picture = new byte[16000],
+                Tags = new[] { ".NET", "node.js", "CLR", "V8", "interop" },
+            };
+
+            return book;
+        }
+    }
+
+    [JSExport]
+    public struct Book
+    {
+        public string Title { get; set; }
+        public Person Author { get; set; }
+        public int Year { get; set; }
+        public double Price { get; set; }
+        public bool Available { get; set; }
+        public string Description { get; set; }
+        public Memory<byte> Picture { get; set; }
+        public string[] Tags { get; set; }
+    }
+
+    [JSExport]
+    public struct Person
+    {
+        public string First { get; set; }
+        public string Last { get; set; }
+    }
+}
---
+++ Test/TestCases/edgejs-perf/measure-latency.js
@@ -0,0 +1,50 @@
+// Ported from https://github.com/agracio/edge-js/blob/master/performance/marshal_clr2v8.js
+
+// Load the addon module, using either hosted or native AOT mode.
+const dotnetModule = process.env['TEST_DOTNET_MODULE_PATH'];
+const dotnetHost = process.env['TEST_DOTNET_HOST_PATH'];
+
+/** @type {import('./edgejs-perf')} */
+const binding = dotnetHost ? require(dotnetHost).require(dotnetModule) : require(dotnetModule);
+
+const callCount = process.env.EDGE_CALL_COUNT || 100000;
+
+const measure = function (func) {
+	var start = Date.now();
+	var i = 0;
+
+	function one() {
+		func({
+			title: 'Run .NET and node.js in-process with edge.js',
+			author: {
+				first: 'Anonymous',
+				last: 'Author'
+			},
+			year: 2013,
+			price: 24.99,
+			available: true,
+			description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus posuere tincidunt felis, et mattis mauris ultrices quis. Cras molestie, quam varius tincidunt tincidunt, mi magna imperdiet lacus, quis elementum ante nibh quis orci. In posuere erat sed tellus lacinia luctus. Praesent sodales tellus mauris, et egestas justo. In blandit, metus non congue adipiscing, est orci luctus odio, non sagittis erat orci ac sapien. Proin ut est id enim mattis volutpat. Vivamus ultrices dapibus feugiat. In dictum tincidunt eros, non pretium nisi rhoncus in. Duis a lacus et elit feugiat ullamcorper. Mauris tempor turpis nulla. Nullam nec facilisis elit.',
+			picture: Buffer.alloc(16000),
+			tags: [ '.NET', 'node.js', 'CLR', 'V8', 'interop']
+		}, function (error, callbck) {
+			if (error) throw error;
+			if (++i < callCount) setImmediate(one);
+			else finish();
+		});
+	}
+
+	function finish() {
+		var delta = Date.now() - start;
+		var result = process.memoryUsage();
+		result.latency = delta / callCount;
+		console.log(result);
+	}
+
+	one();
+};
+
+const invoke = binding.Startup.invoke;
+measure((input, callback) => {
+  const result = invoke(input);
+  callback(null, result);
+});
