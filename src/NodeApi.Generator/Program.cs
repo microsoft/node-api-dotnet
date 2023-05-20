b8aba43e8d22666df93fc0e8efd152fb16597fe5
Jason Ginchereau <jasongin@microsoft.com>
2023-05-19 16:56:45 -1000

    TS generator improvements

--- src/NodeApi.Generator/Program.cs
+++ src/NodeApi.Generator/Program.cs
@@ -3,7 +3,9 @@
 
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 
 namespace Microsoft.JavaScript.NodeApi.Generator;
 
@@ -18,6 +20,13 @@ public static class Program
 {
     private const char PathSeparator = ';';
 
+    private static readonly List<string> s_assemblyPaths = new();
+    private static readonly List<string> s_referenceAssemblyPaths = new();
+    private static readonly List<string> s_typeDefinitionsPaths = new();
+    private static readonly HashSet<int> s_systemAssemblyIndexes = new();
+    private static TypeDefinitionsGenerator.ModuleType s_moduleType;
+    private static bool s_suppressWarnings;
+
     public static int Main(string[] args)
     {
 
@@ -30,50 +39,51 @@ public static class Program
 #pragma warning restore RS1035
 #endif
 
-        if (!ParseArgs(
-            args,
-            out List<string> assemblyPaths,
-            out List<string> referenceAssemblyPaths,
-            out List<string> typeDefinitionsPaths,
-            out bool suppressWarnings))
+        if (!ParseArgs(args))
         {
             Console.WriteLine("Usage: node-api-dotnet-generator [options...]");
             Console.WriteLine();
-            Console.WriteLine("  -a --asssembly   Path to input assembly (required)");
-            Console.WriteLine("  -r --reference   Path to assembly reference by input " +
+            Console.WriteLine("  -a --asssembly  Path to input assembly (required)");
+            Console.WriteLine("  -r --reference  Path to assembly reference by input " +
                 "(optional, multiple)");
-            Console.WriteLine("  -t --typedefs    Path to output type definitions file (required)");
-            Console.WriteLine("  --nowarn         Suppress warnings");
+            Console.WriteLine("  -t --typedefs   Path to output type definitions file (required)");
+            Console.WriteLine("  -m --module     Generate JS loader module(s) alongside typedefs" +
+                "(optional)");
+            Console.WriteLine("                  Valid values are 'commonjs' or 'esm'");
+            Console.WriteLine("  --nowarn        Suppress warnings");
             return 1;
         }
 
-        for (int i = 0; i < assemblyPaths.Count; i++)
+        for (int i = 0; i < s_assemblyPaths.Count; i++)
         {
             // Reference other supplied assemblies, but not the current one.
-            List<string> allReferencePaths = referenceAssemblyPaths
-                .Concat(assemblyPaths.Where((_, j) => j != i)).ToList();
+            List<string> allReferencePaths = s_referenceAssemblyPaths
+                .Concat(s_assemblyPaths.Where((_, j) => j != i)).ToList();
 
-            Console.WriteLine($"{assemblyPaths[i]} -> {typeDefinitionsPaths[i]}");
+            Console.WriteLine($"{s_assemblyPaths[i]} -> {s_typeDefinitionsPaths[i]}");
 
             TypeDefinitionsGenerator.GenerateTypeDefinitions(
-                assemblyPaths[i], allReferencePaths, typeDefinitionsPaths[i], suppressWarnings);
+                s_assemblyPaths[i],
+                allReferencePaths,
+                s_typeDefinitionsPaths[i],
+                s_moduleType,
+                isSystemAssembly: s_systemAssemblyIndexes.Contains(i),
+                s_suppressWarnings);
+
+            if (s_moduleType != TypeDefinitionsGenerator.ModuleType.None)
+            {
+                string pathWithoutExtension = s_typeDefinitionsPaths[i].Substring(
+                    0, s_typeDefinitionsPaths[i].Length - 5);
+                string loaderModulePath = pathWithoutExtension + ".js";
+                Console.WriteLine($"{s_assemblyPaths[i]} -> {loaderModulePath}");
+            }
         }
 
         return 0;
     }
 
-    private static bool ParseArgs(
-        string[] args,
-        out List<string> assemblyPaths,
-        out List<string> referenceAssemblyPaths,
-        out List<string> typeDefinitionsPaths,
-        out bool suppressWarnings)
+    private static bool ParseArgs(string[] args)
     {
-        assemblyPaths = new List<string>();
-        referenceAssemblyPaths = new List<string>();
-        typeDefinitionsPaths = new List<string>();
-        suppressWarnings = false;
-
         for (int i = 0; i < args.Length; i++)
         {
             if (i == args.Length - 1 && args[i] != "--nowarn")
@@ -86,23 +96,42 @@ public static class Program
                 case "-a":
                 case "--assembly":
                 case "--assemblies":
-                    assemblyPaths.AddRange(args[++i].Split(PathSeparator));
+                    s_assemblyPaths.AddRange(args[++i].Split(PathSeparator));
                     break;
 
                 case "-r":
                 case "--reference":
                 case "--references":
-                    referenceAssemblyPaths.AddRange(args[++i].Split(PathSeparator));
+                    s_referenceAssemblyPaths.AddRange(args[++i].Split(PathSeparator));
                     break;
 
                 case "-t":
                 case "--typedef":
                 case "--typedefs":
-                    typeDefinitionsPaths.AddRange(args[++i].Split(PathSeparator));
+                    s_typeDefinitionsPaths.AddRange(args[++i].Split(PathSeparator));
+                    break;
+
+                case "-m":
+                case "--module":
+                case "--modules":
+                    string moduleType = args[++i].ToLowerInvariant();
+                    switch (moduleType)
+                    {
+                        case "es":
+                        case "esm":
+                        case "mjs":
+                            s_moduleType = TypeDefinitionsGenerator.ModuleType.ES;
+                            break;
+                        case "commonjs":
+                        case "cjs":
+                            s_moduleType = TypeDefinitionsGenerator.ModuleType.CommonJS;
+                            break;
+                        default: return false;
+                    }
                     break;
 
                 case "--nowarn":
-                    suppressWarnings = true;
+                    s_suppressWarnings = true;
                     break;
 
                 default:
@@ -111,32 +140,97 @@ public static class Program
             }
         }
 
-        if (assemblyPaths.Any(
+        ResolveSystemAssemblies();
+
+        if (s_assemblyPaths.Any(
                 (a) => !a.EndsWith(".dll", StringComparison.OrdinalIgnoreCase)) ||
-            referenceAssemblyPaths.Any(
+            s_referenceAssemblyPaths.Any(
                 (r) => !r.EndsWith(".dll", StringComparison.OrdinalIgnoreCase)) ||
-            typeDefinitionsPaths.Any(
+            s_typeDefinitionsPaths.Any(
                 (t) => !t.EndsWith(".d.ts", StringComparison.OrdinalIgnoreCase)))
         {
-            Console.WriteLine("Incorrect file extension.");
+            Console.WriteLine("Incorrect file path or extension.");
             return false;
         }
-        else if (assemblyPaths.Count == 0)
+        else if (s_assemblyPaths.Count == 0)
         {
             Console.WriteLine("Specify an assembly file path.");
             return false;
         }
-        else if (typeDefinitionsPaths.Count == 0)
+        else if (s_typeDefinitionsPaths.Count == 0)
         {
             Console.WriteLine("Specify a type definitions file path.");
             return false;
         }
-        else if (typeDefinitionsPaths.Count != assemblyPaths.Count)
+        else if (s_typeDefinitionsPaths.Count != s_assemblyPaths.Count)
         {
             Console.WriteLine("Specify a type definitions file path for every assembly.");
             return false;
         }
 
         return true;
+    }
+
+    private static void ResolveSystemAssemblies()
+    {
+        string systemAssemblyDirectory = RuntimeEnvironment.GetRuntimeDirectory();
+        if (systemAssemblyDirectory[systemAssemblyDirectory.Length - 1] ==
+            Path.DirectorySeparatorChar)
+        {
+            systemAssemblyDirectory = systemAssemblyDirectory.Substring(
+                0, systemAssemblyDirectory.Length - 1);
+        }
+
+        string runtimeVersionDir = Path.GetFileName(systemAssemblyDirectory);
+        string dotnetRootDirectory = Path.GetDirectoryName(Path.GetDirectoryName(
+            Path.GetDirectoryName(systemAssemblyDirectory)!)!)!;
+        string tfm = GetCurrentFrameworkTarget();
+
+        string refAssemblyDirectory = Path.Combine(
+            dotnetRootDirectory,
+            "packs",
+            "Microsoft.NETCore.App.Ref",
+            runtimeVersionDir,
+            "ref",
+            tfm);
+
+        for (int i = 0; i < s_assemblyPaths.Count; i++)
+        {
+            if (string.IsNullOrEmpty(s_assemblyPaths[i]) &&
+                s_typeDefinitionsPaths.Count == s_assemblyPaths.Count)
+            {
+                // Skip empty items, which might come from concatenating MSBuild item lists.
+                s_assemblyPaths.RemoveAt(i);
+                s_typeDefinitionsPaths.RemoveAt(i);
+                i--;
+                continue;
+            }
+
+            if (!s_assemblyPaths[i].EndsWith(".dll", StringComparison.OrdinalIgnoreCase))
+            {
+                string systemAssemblyPath = Path.Combine(
+                    refAssemblyDirectory, s_assemblyPaths[i] + ".dll");
+#pragma warning disable RS1035 // Do not use APIs banned for analyzers
+                if (File.Exists(systemAssemblyPath))
+#pragma warning restore RS1035
+                {
+                    s_assemblyPaths[i] = systemAssemblyPath;
+                    s_systemAssemblyIndexes.Add(i);
+                }
+                else
+                {
+                    Console.WriteLine("System assembly not found at " + systemAssemblyPath);
+                }
+            }
+        }
+    }
+
+    public static string GetCurrentFrameworkTarget()
+    {
+#pragma warning disable RS1035 // Do not use APIs banned for analyzers
+        Version frameworkVersion = Environment.Version;
+#pragma warning restore RS1035
+        return frameworkVersion.Major == 4 ? "net472" :
+            $"net{frameworkVersion.Major}.{frameworkVersion.Minor}";
     }
 }
