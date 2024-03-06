68e1db64e503ee23de20930247a4474bd0fa3623
Jason Ginchereau <jasongin@microsoft.com>
2024-03-05 14:12:14 -1000

    Fix multi-instance test

--- test/TestCases/napi-dotnet/multi_instance.js
+++ test/TestCases/napi-dotnet/multi_instance.js
@@ -8,6 +8,9 @@ const { Worker, isMainThread, parentPort } = require('worker_threads');
 /** @type Binding */
 const binding = require('../common').binding;
 
+const testModulePath = require('../common').testModulePath;
+const isAot = /\.node$/.test(testModulePath);
+
 if (isMainThread) {
   // Increment the static counter to 2.
   const count1 = binding.Counter.count();
@@ -15,10 +18,11 @@ if (isMainThread) {
   assert.strictEqual(binding.Counter.count(), 2);
 
   // AOT modules do not get reloaded when the node module is rebound, so their static data is
-  // not isolated. But .NET hosted modules do get reloaded in a worker, with isolated static data,
-  // so the following assertions only pass for that type of module.
+  // not isolated across threads. But .NET hosted modules do get reloaded with isolated static data,
+  // so in that case the worker-thread counter should be independent.
+  const expectedCount = isAot ? 3 : 1;
   const worker = new Worker(__filename);
-  worker.on('message', (count3) => assert.strictEqual(count3, 1));
+  worker.on('message', (count3) => assert.strictEqual(count3, expectedCount));
 } else {
   const count3 = binding.Counter.count();
   parentPort.postMessage(count3)
