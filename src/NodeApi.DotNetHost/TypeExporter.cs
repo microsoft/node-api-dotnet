5d321c9bbfff08845a2b5be4e0f4daa31cd5d994
Dmitriy Ivanov <147907089+DmitriySft@users.noreply.github.com>
2024-04-09 05:37:45 +1200

    Fix for types derived from un-namespaced types. (#261)

--- src/NodeApi.DotNetHost/TypeExporter.cs
+++ src/NodeApi.DotNetHost/TypeExporter.cs
@@ -212,7 +212,7 @@ public class TypeExporter
         {
             string baseOrInterfaceTypeName = TypeProxy.GetTypeProxyName(baseOrInterfaceType);
 
-            NamespaceProxy? ns = GetNamespaceProxy(baseOrInterfaceType.Namespace!);
+            NamespaceProxy? ns = GetNamespaceProxy(baseOrInterfaceType.Namespace);
             if (ns == null)
             {
                 Trace(
@@ -246,7 +246,7 @@ public class TypeExporter
         // Target namespaces and types should be already loaded because either they are in the
         // current assembly (where types are loaded before extension methods) or in an assembly
         // this one depends on which would have been loaded already.
-        NamespaceProxy? targetTypeNamespace = GetNamespaceProxy(targetType.Namespace!);
+        NamespaceProxy? targetTypeNamespace = GetNamespaceProxy(targetType.Namespace);
         if (targetTypeNamespace == null)
         {
             Trace(
@@ -308,8 +308,13 @@ public class TypeExporter
         return true;
     }
 
-    internal NamespaceProxy? GetNamespaceProxy(string ns)
+    internal NamespaceProxy? GetNamespaceProxy(string? ns)
     {
+        if (ns is null)
+        {
+            return null;
+        }
+
         string[] namespaceParts = ns.Split('.');
         if (!_exportedNamespaces.TryGetValue(
             namespaceParts[0], out NamespaceProxy? namespaceProxy))
---
+++ test/TestCases/napi-dotnet/NoNamespaceTypes.cs
@@ -0,0 +1,66 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System.Collections;
+using System.Collections.Generic;
+
+#pragma warning disable IDE0060 // Remove unused parameter
+#pragma warning disable CA1050 // Declare types in namespaces
+#pragma warning disable CA1822 // Mark members as static
+
+public class NoNamespaceType
+{
+}
+
+public interface INoNamespaceInterface
+{
+}
+
+public class NoNamespaceCollection : ICollection<string>
+{
+    public IEnumerator<string> GetEnumerator() => throw new System.NotImplementedException();
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+    public void Add(string item) => throw new System.NotImplementedException();
+
+    public void Clear() => throw new System.NotImplementedException();
+
+    public bool Contains(string item) => throw new System.NotImplementedException();
+
+    public void CopyTo(string[] array, int arrayIndex) => throw new System.NotImplementedException();
+
+    public bool Remove(string item) => throw new System.NotImplementedException();
+
+    public int Count { get; }
+    public bool IsReadOnly { get; }
+}
+
+public delegate void NoNamespaceDelegate();
+
+namespace Microsoft.JavaScript.NodeApi.TestCases
+{
+    public class NoNamespaceInterfaceImpl : INoNamespaceInterface
+    {
+    }
+
+    public class NoNamespaceTypeImpl : NoNamespaceType
+    {
+    }
+
+    public class NoNamespaceContainer
+    {
+        public static NoNamespaceDelegate DelegateProperty { get; set; }
+
+        public List<NoNamespaceType>? GetList(INoNamespaceInterface arg)
+        {
+            return null;
+        }
+
+        public T GetList<T>()
+            where T : List<NoNamespaceType>, INoNamespaceInterface
+        {
+            return default!;
+        }
+    }
+}
